2cb9232cab5e1405f083e84efd5f2059f8f2502b
Anzor <soldaev.anzor@gmail.com>
2024-12-16 20:13:20 +0100

    16/12/2024 FINISHED -all of recap done -well placed filters

--- recap.php
+++ recap.php
@@ -24,14 +24,24 @@
                             '<th>Quantity</th>',
                             '<th>Total</th>',
                         '</tr>',
-                    '</thead>'
+                    '</thead>',
                     '<tbody>';
-             
-                    
- 
-                    
-
-
+        $totalGeneral = 0;
+        foreach($_SESSION['products'] as $index => $product){
+            echo '<tr>',
+                    '<td>'. $index . '</td>',                    
+                    '<td>'. $product ['name']. '</td>',                    
+                    '<td>'. number_format($product['price'], 2 , ',', '&nbsp;'). '&nbsp;£'. '</td>',                    
+                    '<td>'. $product['qtt']. '</td>',                    
+                    '<td>'. number_format($product['price'], 2 , ',', '&nbsp;'). '&nbsp;£'. '</td>',
+                 '</tr>';    
+                $totalGeneral += $product['total'];                
+        }
+        echo '<tr>',
+                '<td colspan = 4> Total general : </td>',
+                '<td> <strong>'.number_format($totalGeneral, 2 , ',' , '&nbsp;'). '&nbsp;£</strong> </td>',
+              '</tbody>',
+            '</table>';        
         
         }
     ?>
