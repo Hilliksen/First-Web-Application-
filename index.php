56675aad08de9ef832baedbf72deb637b8dc1305
Anzor <soldaev.anzor@gmail.com>
2024-12-16 14:58:00 +0100

    16/12/2024 -Made necessary files for the project

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+</head>
+<body>
+
+    <h1>Add a product</h1>
+    <form action="processing.php" method = "post">
+        <p>
+            <label>
+                Name of the product :
+                <input type = "text" name = "name">
+            </label>
+        </p>
+
+        <p>
+            <label> 
+                Price of the product :
+                <input type = "number" steps = "any" name = "price"> 
+            </label>
+        </p>
+
+        <p>
+            <label> 
+                Desired quantity : 
+                <input type = "name" name = "qtt" value = "1">
+            </label>
+        </p>
+        <p>
+            <input type = "submit" name = "submit" value = "Add the product">
+        </p>
+    </form>
+
+
+</body>
+</html>
