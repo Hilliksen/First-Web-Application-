107c6267a11d016fb0270b109d1f76dea3e2882f
Anzor <soldaev.anzor@gmail.com>
2024-12-17 11:53:41 +0100

    - Structure for styling mostly done (Will add more functions later)

--- index.php
+++ index.php
@@ -3,37 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Product</title>
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Teleshopping</title>
 </head>
 <body>
 
-    <h1>Add a product</h1>
-    <form action="processing.php" method = "post">
-        <p>
-            <label>
-                Name of the product :
-                <input type = "text" name = "name">
-            </label>
-        </p>
 
-        <p>
-            <label> 
-                Price of the product :
-                <input type = "number" step = "0.1" name = "price"> 
-            </label>
-        </p>
+<div class="nav">
+    <a href="recap.php">CLICK ></a>
+</div>
 
-        <p>
-            <label> 
-                Desired quantity : 
-                <input type = "name" name = "qtt" value = "1">
-            </label>
-        </p>
-        <p>
-            <input type = "submit" name = "submit" value = "Add the product">
-        </p>
-    </form>
 
+
+    <h1>ADD A PRODUCT</h1>
+    <div id="container">
+        <form action="processing.php" method = "post">
+            <p>
+                <label>
+                    Name of the product : <br>
+                    <input type = "text" name = "name">
+                </label>
+            </p>
+
+            <p>
+                <label>
+                    Price of the product : <br>
+                    <input type = "number" step = "0.1" name = "price"> 
+                </label>
+            </p>
+
+            <p>
+                <label> 
+                    Desired quantity : <br>
+                    <input type = "name" name = "qtt" value = "1">
+                </label>
+            </p>
+            <p>
+                <input type = "submit" name = "submit" value = "SUBMIT">
+            </p>
+        </form>
+    </div>
 
 </body>
 </html>
