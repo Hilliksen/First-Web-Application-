ee86b8fa47cb51dfd526b0362f9f0332a486d917
Anzor <soldaev.anzor@gmail.com>
2024-12-17 17:03:58 +0100

    17/12/2024 added an icon arrow and fix a little thing in the table, wrong value given in second number format

--- recap.php
+++ recap.php
@@ -7,16 +7,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheets" href="css/index.css">
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
+    <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="css/index.css"> 
     <title>Recap of products</title>
 </head>
 <body>
-
+    <div class="nav2">
+        <a href="index.php"><i class="fa-solid fa-arrow-left"></i>GO BACK</a>
+    </div>
+    <h1>YOUR PRODUCTS</h1>
     <div id="container">
         
         <?php 
             if(!isset($_SESSION['products']) || empty($_SESSION['products'])){  //# If session[products] is NOT set (the !) or its empty meaning no products were put in 
-                echo '<p> No products selected....</p>'; //# then we echo this
+                echo "<p class ='error'> No products selected....</p>"; //# then we echo this
             }
             else{
                 echo '<table>',   //# Here we just make a table containing our products
@@ -37,7 +42,8 @@
                         '<td>'. $product ['name']. '</td>',                    
                         '<td>'. number_format($product['price'], 2 , ',', '&nbsp;'). '&nbsp;£'. '</td>',  //# We use number formatter to change the way our value is presented, so we start by getting the value first, then by saying 2 you mean 2 places after a decimal, next we show what sign is for decimal for us and after that we add a nonbreaking space. After we formatted our value we just add a symbol for money                   
                         '<td>'. $product['qtt']. '</td>',                    
-                        '<td>'. number_format($product['price'], 2 , ',', '&nbsp;'). '&nbsp;£'. '</td>',
+                        '<td>'. number_format($product['total'], 2 , ',', '&nbsp;'). '&nbsp;£'. '</td>',
+                        '<td> X </td>',
                         '</tr>';    
                     $totalGeneral += $product['total'];                
             }
