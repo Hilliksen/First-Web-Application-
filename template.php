4a5082e700d273f82422c3f4f01caf60ced57223
Anzor <soldaev.anzor@gmail.com>
2024-12-20 14:38:13 +0100

    CREATED NEW PAGE THAT LETS US CUSTOMAZE ETC

---
+++ template.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'>
+    <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Teleshopping</title>
+</head>
+<body>
+    <div class="wrapper">
+        <nav>
+            <ul>
+                <li><a href="">Page 1</a></li>
+                <li><a href="">Page 2</a></li>
+                <li><a href="">Page 3</a></li>
+            </ul>
+        </nav>
+
+        <?=$content?>
+    
+    </div>
+
+    <footer>Anzor</footer>
+</body>
+</html>
